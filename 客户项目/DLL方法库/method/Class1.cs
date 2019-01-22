afd245e2bd0b7003b039dc2e123807ac5bff3cc8
Your Name <you@example.com>
2019-01-22 14:05:17 +0800

    无忧考网

---
+++ 客户项目/DLL方法库/method/Class1.cs
@@ -0,0 +1,177 @@
+﻿
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace method
+{
+    public class methodClass
+    {
+        #region GET请求
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <param name="charset"></param>
+        /// <param name="COOKIE"></param>
+        /// <returns></returns>
+        public static string GetUrl(string Url, string charset,string COOKIE)
+        {
+            try
+            {
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);  //创建一个链接
+
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+                request.AllowAutoRedirect = true;
+                request.Headers.Add("Cookie", COOKIE);
+                request.KeepAlive = false;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;  //获取反馈
+
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(charset)); //reader.ReadToEnd() 表示取得网页的源码流 需要引用 using  IO
+
+                string content = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+                return content;
+
+            }
+            catch (System.Exception ex)
+            {
+                ex.ToString();
+
+            }
+            return "";
+        }
+        #endregion
+
+
+        #region listview转datable
+        /// <summary>
+        /// listview转datable
+        /// </summary>
+        /// <param name="lv"></param>
+        /// <returns></returns>
+        public static DataTable listViewToDataTable(ListView lv)
+        {
+            int i, j;
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.Clear();
+            dt.Columns.Clear();
+            //生成DataTable列头
+            for (i = 0; i < lv.Columns.Count; i++)
+            {
+                dt.Columns.Add(lv.Columns[i].Text.Trim(), typeof(String));
+            }
+            //每行内容
+            for (i = 0; i < lv.Items.Count; i++)
+            {
+                dr = dt.NewRow();
+                for (j = 0; j < lv.Columns.Count; j++)
+                {
+                    dr[j] = lv.Items[i].SubItems[j].Text.Trim();
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+        #endregion
+
+
+        #region NPOI导出表格
+        public static int DataTableToExcel(DataTable data, string sheetName, bool isColumnWritten)
+        {
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            ISheet sheet = null;
+            IWorkbook workbook = null;
+            FileStream fs = null;
+            // bool disposed;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "xlsx|*.xls|xlsx|*.xlsx";
+            sfd.Title = "Excel文件导出";
+            string fileName = "";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = sfd.FileName;
+            }
+            else
+                return -1;
+
+            fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            if (fileName.IndexOf(".xlsx") > 0) // 2007版本
+                workbook = new XSSFWorkbook();
+            else if (fileName.IndexOf(".xls") > 0) // 2003版本
+                workbook = new HSSFWorkbook();
+
+            try
+            {
+                if (workbook != null)
+                {
+                    sheet = workbook.CreateSheet(sheetName);
+                    ICellStyle style = workbook.CreateCellStyle();
+                    style.FillPattern = FillPattern.SolidForeground;
+
+                }
+                else
+                {
+                    return -1;
+                }
+
+                if (isColumnWritten == true) //写入DataTable的列名
+                {
+                    IRow row = sheet.CreateRow(0);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Columns[j].ColumnName);
+
+                    }
+                    count = 1;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+                for (i = 0; i < data.Rows.Count; ++i)
+                {
+                    IRow row = sheet.CreateRow(count);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Rows[i][j].ToString());
+                    }
+                    ++count;
+                }
+                workbook.Write(fs); //写入到excel
+                workbook.Close();
+                fs.Close();
+                System.Diagnostics.Process[] Proc = System.Diagnostics.Process.GetProcessesByName("");
+                MessageBox.Show("数据导出完成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                return -1;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ 客户项目/无忧考网/wuyou/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using method;
+
+namespace wuyou
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void run()
+        {
+            try
+            {
+                
+
+                    string[] keywords = textBox1.Text.Split(new string[] { "," }, StringSplitOptions.None);
+
+                foreach (string keyword in keywords)
+                {
+
+                string url = "http://search.51test.net/cse/search?q="+keyword+"&p=0&s=10721678224061263659&entry=1";
+                    
+                    string html = methodClass.GetUrl(url, "utf-8", "");
+                    
+                    MatchCollection urls = Regex.Matches(html, @"cpos=""title"" href=""([\s\S]*?)""", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                    
+                    string strhtml = methodClass.GetUrl(urls[1].Groups[1].Value, "gb2312", "");
+
+                     MatchCollection matches = Regex.Matches(strhtml, @"<h2><a href=""([\s\S]*?)""", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+                    ArrayList lists = new ArrayList();
+                    foreach (Match match in matches)
+                    {
+                        lists.Add("http://www.51test.net"+match.Groups[1].Value.Trim());
+                    }
+
+                    MessageBox.Show(lists.Count.ToString());
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        string ahtml = methodClass.GetUrl(lists[i].ToString(), "gb2312", "");
+
+                        Match title = Regex.Match(ahtml, @"<title>([\s\S]*?)</title>");
+                        Match body = Regex.Match(ahtml, @"<div class=""content-txt"">([\s\S]*?)<div class=""content_download"">");
+                        ListViewItem lv1 = this.listView1.Items.Add(title.Groups[1].Value);
+                        lv1.SubItems.Add(body.Groups[1].Value.Replace("<div class=\"describe\"><i></i>", ""));
+
+
+                        if (listView1.Items.Count > 1)
+                        {
+                            this.listView1.EnsureVisible(this.listView1.Items.Count - 1);
+                        }
+                        
+                        Application.DoEvents();
+                        Thread.Sleep(1000);
+                    }
+                    
+
+ 
+
+                    }
+
+                }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(this.run));
+            Control.CheckForIllegalCrossThreadCalls = false;
+            thread.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                MessageBox.Show("列表为空!");
+            }
+            else
+            {
+                List<string> list = new List<string>();
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    string temp = item.SubItems[0].Text;
+                    list.Add(temp + "\r\n"+item.SubItems[1].Text);
+                }
+                Thread thexp = new Thread(() => export(list)) { IsBackground = true };
+                thexp.Start();
+            }
+        }
+
+        private void export(List<string> list)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + Guid.NewGuid().ToString() + ".docx";
+
+            //删除txt文本
+            string path1 = Environment.CurrentDirectory;
+            string pattern = "*.txt";
+            string[] strFileName = Directory.GetFiles(path1, pattern);
+            foreach (var item in strFileName)
+            {
+                File.Delete(item);
+
+            }
+            //删除txt文本
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string tel in list)
+            {
+                sb.AppendLine(tel);
+            }
+            System.IO.File.WriteAllText(path, sb.ToString(), Encoding.UTF8);
+            MessageBox.Show("导出完成！存放地址为程序文件夹");
+
+        }
+
+    }
+}
