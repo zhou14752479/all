ffbe7a04ebed078caf19e665b9ab2e7e76cedb53
Your Name <you@example.com>
2019-07-21 19:57:46 +0800

    7-21

--- 客户项目/项目集合/main/2019_7/网易充值.Designer.cs
+++ 客户项目/项目集合/main/2019_7/网易充值.Designer.cs
@@ -28,17 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(网易充值));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button5 = new System.Windows.Forms.Button();
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.listView1 = new System.Windows.Forms.ListView();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -48,17 +49,6 @@
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // button5
-            // 
-            this.button5.ForeColor = System.Drawing.Color.Blue;
-            this.button5.Location = new System.Drawing.Point(3, 3);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(90, 26);
-            this.button5.TabIndex = 16;
-            this.button5.Text = "登陆账号";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // columnHeader2
             // 
@@ -79,7 +69,7 @@
             this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(249, 235);
+            this.listView1.Size = new System.Drawing.Size(249, 194);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -87,7 +77,7 @@
             // button2
             // 
             this.button2.ForeColor = System.Drawing.Color.Blue;
-            this.button2.Location = new System.Drawing.Point(133, 61);
+            this.button2.Location = new System.Drawing.Point(133, 84);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(90, 26);
             this.button2.TabIndex = 11;
@@ -98,7 +88,7 @@
             // button1
             // 
             this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(3, 59);
+            this.button1.Location = new System.Drawing.Point(3, 82);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 28);
             this.button1.TabIndex = 10;
@@ -120,32 +110,31 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
             this.splitContainer1.Panel2.Controls.Add(this.button4);
             this.splitContainer1.Panel2.Controls.Add(this.button3);
-            this.splitContainer1.Panel2.Controls.Add(this.button5);
             this.splitContainer1.Panel2.Controls.Add(this.button2);
             this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.MouseEnter += new System.EventHandler(this.splitContainer1_Panel2_MouseEnter);
             this.splitContainer1.Size = new System.Drawing.Size(249, 369);
-            this.splitContainer1.SplitterDistance = 235;
+            this.splitContainer1.SplitterDistance = 194;
             this.splitContainer1.TabIndex = 8;
             // 
-            // button3
+            // label1
             // 
-            this.button3.ForeColor = System.Drawing.Color.Blue;
-            this.button3.Location = new System.Drawing.Point(3, 92);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 28);
-            this.button3.TabIndex = 17;
-            this.button3.Text = "暂停";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 11F);
+            this.label1.Location = new System.Drawing.Point(0, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "未启动";
             // 
             // button4
             // 
             this.button4.ForeColor = System.Drawing.Color.Blue;
-            this.button4.Location = new System.Drawing.Point(133, 92);
+            this.button4.Location = new System.Drawing.Point(133, 115);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(90, 28);
             this.button4.TabIndex = 18;
@@ -153,15 +142,25 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 11F);
-            this.label1.Location = new System.Drawing.Point(3, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 15);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "未启动";
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(3, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 28);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "暂停";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 73);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
             // 网易充值
             // 
@@ -172,6 +171,7 @@
             this.Name = "网易充值";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "网易充值";
+            this.Load += new System.EventHandler(this.网易充值_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
@@ -184,7 +184,6 @@
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ListView listView1;
@@ -194,5 +193,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- 客户项目/项目集合/main/2019_7/网易充值.cs
+++ 客户项目/项目集合/main/2019_7/网易充值.cs
@@ -19,19 +19,14 @@ namespace main._2019_7
             InitializeComponent();
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            谷歌浏览器 web = new 谷歌浏览器("http://ecard.163.com/index_login?refer_uri=%2Findex");
-          
-            web.Show();
-        }
+     
 
         public static string COOKIE;
         bool zanting = true;
 
         private void splitContainer1_Panel2_MouseEnter(object sender, EventArgs e)
         {
-           COOKIE = webBrowser.cookie;
+           
 
         }
 
@@ -40,7 +35,7 @@ namespace main._2019_7
         #region 主程序
         public void run()
         {
-
+            COOKIE = textBox1.Text;
             try
             {
                 for (int i = 1; i < 9999; i++)
@@ -51,15 +46,27 @@ namespace main._2019_7
                     string html = method.GetUrlWithCookie(Url,COOKIE ,"gb2312");
                     
                     MatchCollection ids = Regex.Matches(html, @"<td class=""red"">([\s\S]*?)<br>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
-                   
+
                     if (ids.Count == 0)
+                    {
+                        label1.Text = "抓取结束";
                         return;
-                    label1.Text = "正在抓取第"+i+"页";
+                        
+                    }
+                        
+                    label1.Text = "正在抓取第"+i+"页，未发现充值";
                     for (int j = 0; j < ids.Count; j++)
                     {
-                        ListViewItem lv1 = listView1.Items.Add((listView1.Items.Count).ToString()); //使用Listview展示数据         
-                        lv1.SubItems.Add(ids[j].Groups[1].Value.Trim());
+                        if (ids[j].Groups[1].Value.Contains("充值"))
+                        {
 
+                            ListViewItem lv1 = listView1.Items.Add((listView1.Items.Count).ToString()); //使用Listview展示数据         
+                            lv1.SubItems.Add(ids[j].Groups[1].Value.Trim());
+                            if (listView1.Items.Count > 2)
+                            {
+                                listView1.EnsureVisible(listView1.Items.Count - 1);  //滚动到指定位置
+                            }
+                        }
                     }
 
                     while (this.zanting == false)
@@ -67,7 +74,7 @@ namespace main._2019_7
                             Application.DoEvents();//如果loader是false表明正在加载,,则Application.DoEvents()意思就是处理其他消息。阻止当前的队列继续执行。
                         }
                     
-                        Thread.Sleep(1000);
+                        //Thread.Sleep(1000);
                     }
 
                 }
@@ -103,6 +110,11 @@ namespace main._2019_7
         private void button2_Click(object sender, EventArgs e)
         {
             method.DataTableToExcel(method.listViewToDataTable(this.listView1), "Sheet1", true);
+        }
+
+        private void 网易充值_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
