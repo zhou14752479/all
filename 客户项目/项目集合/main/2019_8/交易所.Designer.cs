d4142dc2de9354ea09875690ca19062dec1dc5da
zhoukaige00 <852266010@qq.com>
2019-09-06 17:37:21 +0800

    9---6

--- 客户项目/项目集合/main/2019_8/交易所.Designer.cs
+++ 客户项目/项目集合/main/2019_8/交易所.Designer.cs
@@ -32,11 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -45,15 +45,16 @@
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -71,7 +72,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.Maroon;
-            this.label1.Location = new System.Drawing.Point(12, 170);
+            this.label1.Location = new System.Drawing.Point(180, 170);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 15);
             this.label1.TabIndex = 32;
@@ -101,35 +102,16 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "操作区";
             // 
-            // button1
+            // button3
             // 
-            this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(245, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 68);
-            this.button1.TabIndex = 31;
-            this.button1.Text = "开始采集";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.ForeColor = System.Drawing.Color.Blue;
-            this.button2.Location = new System.Drawing.Point(130, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(109, 26);
-            this.button2.TabIndex = 32;
-            this.button2.Text = "导出信息";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.ForeColor = System.Drawing.Color.Blue;
-            this.button5.Location = new System.Drawing.Point(15, 74);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(109, 26);
-            this.button5.TabIndex = 37;
-            this.button5.Text = "清空数据";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button3.ForeColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(15, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 26);
+            this.button3.TabIndex = 34;
+            this.button3.Text = "暂停";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // button4
             // 
@@ -140,16 +122,40 @@
             this.button4.TabIndex = 33;
             this.button4.Text = "继续";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
             // 
-            // button3
+            // button5
             // 
-            this.button3.ForeColor = System.Drawing.Color.Blue;
-            this.button3.Location = new System.Drawing.Point(15, 32);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(109, 26);
-            this.button3.TabIndex = 34;
-            this.button3.Text = "暂停";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button5.ForeColor = System.Drawing.Color.Blue;
+            this.button5.Location = new System.Drawing.Point(15, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 26);
+            this.button5.TabIndex = 37;
+            this.button5.Text = "清空数据";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(130, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 26);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "导出信息";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(245, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 68);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "开始采集";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // listView1
             // 
@@ -165,7 +171,7 @@
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(866, 288);
+            this.listView1.Size = new System.Drawing.Size(862, 403);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -198,7 +204,7 @@
             // columnHeader6
             // 
             this.columnHeader6.Text = "链接";
-            this.columnHeader6.Width = 300;
+            this.columnHeader6.Width = 500;
             // 
             // splitContainer1
             // 
@@ -214,30 +220,65 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
             this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
             this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
             this.splitContainer1.Panel2.Controls.Add(this.label5);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
-            this.splitContainer1.Size = new System.Drawing.Size(866, 486);
-            this.splitContainer1.SplitterDistance = 288;
+            this.splitContainer1.Size = new System.Drawing.Size(862, 681);
+            this.splitContainer1.SplitterDistance = 403;
             this.splitContainer1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("黑体", 10F, System.Drawing.FontStyle.Bold);
+            this.textBox2.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox2.Location = new System.Drawing.Point(273, 169);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(581, 92);
+            this.textBox2.TabIndex = 49;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.dateTimePicker2);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.dateTimePicker1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 17);
+            this.groupBox2.Location = new System.Drawing.Point(183, 17);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(449, 133);
+            this.groupBox2.Size = new System.Drawing.Size(315, 133);
             this.groupBox2.TabIndex = 40;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "参数设置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 12);
+            this.label2.TabIndex = 49;
+            this.label2.Text = "股票代码：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("黑体", 10F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox1.Location = new System.Drawing.Point(88, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(74, 231);
+            this.textBox1.TabIndex = 48;
+            this.textBox1.Text = "000027";
             // 
             // label6
             // 
@@ -299,37 +340,16 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(111, 21);
             this.dateTimePicker1.TabIndex = 42;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.ForeColor = System.Drawing.Color.Maroon;
-            this.label2.Location = new System.Drawing.Point(339, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 12);
-            this.label2.TabIndex = 49;
-            this.label2.Text = "股票代码：";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("黑体", 10F, System.Drawing.FontStyle.Bold);
-            this.textBox1.ForeColor = System.Drawing.Color.Maroon;
-            this.textBox1.Location = new System.Drawing.Point(342, 44);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(74, 68);
-            this.textBox1.TabIndex = 48;
-            this.textBox1.Text = "06862\r\n00520\r\n00052";
-            // 
             // 交易所
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(866, 486);
+            this.ClientSize = new System.Drawing.Size(862, 681);
             this.Controls.Add(this.splitContainer1);
             this.Name = "交易所";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "交易所";
+            this.Load += new System.EventHandler(this.交易所_Load);
             this.groupBox1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -369,5 +389,6 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- 客户项目/项目集合/main/2019_8/交易所.cs
+++ 客户项目/项目集合/main/2019_8/交易所.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +19,282 @@ namespace main._2019_8
         public 交易所()
         {
             InitializeComponent();
+        }
+        #region GET请求
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="Url">网址</param>
+        /// <returns></returns>
+        public static string GetUrl(string Url, string referUrl)
+        {
+            try
+            {
+                          
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);  //创建一个链接
+                
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36";
+                request.Referer = referUrl;
+                request.AllowAutoRedirect = true;
+               
+                request.KeepAlive = true;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;  //获取反馈
+
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")); //reader.ReadToEnd() 表示取得网页的源码流 需要引用 using  IO
+
+                string content = reader.ReadToEnd();
+
+                reader.Close();
+                response.Close();
+                return content;
+
+            }
+            catch (System.Exception ex)
+            {
+                ex.ToString();
+
+            }
+            return "";
+        }
+        #endregion
+
+        #region POST请求
+        /// <summary>
+        /// POST请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送的数据包</param>
+        /// <param name="COOKIE">cookie</param>
+        /// <param name="charset">编码格式</param>
+        /// <returns></returns>
+        public static string PostUrl(string url, string postData)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "Post";
+            //request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentType = "application/json";
+            request.ContentLength = postData.Length;
+            request.AllowAutoRedirect = false;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36";
+           
+            StreamWriter sw = new StreamWriter(request.GetRequestStream());
+            sw.Write(postData);
+            sw.Flush();
+
+
+            WebResponse response = request.GetResponse();
+            Stream s = response.GetResponseStream();
+            StreamReader sr = new StreamReader(s, Encoding.GetEncoding("utf-8"));
+            string html = sr.ReadToEnd();
+
+            sw.Dispose();
+            sw.Close();
+            sr.Dispose();
+            sr.Close();
+            s.Dispose();
+            s.Close();
+            return html;
+        }
+
+        #endregion
+
+        bool zanting = true;
+        private void 交易所_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string getName(string code)
+        {
+            Match name = Regex.Match(method.GetUrl("https://suggest3.sinajs.cn/suggest/type=&key="+code+"&name=suggestdata_1567749930682", "gb2312"), @"=""([\s\S]*?),");
+            return (name.Groups[1].Value);
+        }
+
+        #region 上海证券交易所
+
+        public void sse()
+        {
+            DateTime dtStart = DateTime.Parse(dateTimePicker1.Text); ;
+            DateTime dtEnd = DateTime.Parse(dateTimePicker2.Text);
+
+            string startdate = dtStart.ToString("yyyy-MM-dd");
+            string enddate = dtEnd.ToString("yyyy-MM-dd");
+
+            string[] text = textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            for (int i= 0; i < text.Length; i++)
+            {
+                string name = getName(text[i]);
+                for (int j= 1; j < 50; j++)
+                {
+                    string url = "http://query.sse.com.cn/security/stock/queryCompanyBulletin.do?jsonCallBack=jsonpCallback56167&isPagination=true&productId="+text[i]+"&keyWord=&securityType=0101%2C120100%2C020100%2C020200%2C120200&reportType2=&reportType=ALL&beginDate="+startdate+"&endDate="+enddate+"&pageHelp.pageSize=25&pageHelp.pageCount=50&pageHelp.pageNo="+j+"&pageHelp.beginPage="+j+"&pageHelp.cacheSize=1&pageHelp.endPage=50&_=1567736111023";
+                    string referUrl = "http://www.sse.com.cn/disclosure/listedinfo/announcement/";
+                    
+                    string html = GetUrl(url,referUrl);
+                    textBox2.Text += DateTime.Now.ToString() + "正在抓取........" + text[i] + "\r\n";
+
+                    MatchCollection dates = Regex.Matches(html, @"""SSEDATE"":""([\s\S]*?)""");
+                  
+                    MatchCollection titles = Regex.Matches(html, @"""TITLE"":""([\s\S]*?)""");
+                    MatchCollection links = Regex.Matches(html, @"""URL"":""([\s\S]*?)""");
+
+                    if (dates.Count == 0)
+                        break;
+
+      
+                        for (int a = 0; a < dates.Count; a++)
+                        {
+
+                            ListViewItem lv1 = listView1.Items.Add((listView1.Items.Count + 1).ToString()); //使用Listview展示数据         
+                            lv1.SubItems.Add(dates[a].Groups[1].Value);
+                            lv1.SubItems.Add(text[i]);
+                            lv1.SubItems.Add(name);
+                            lv1.SubItems.Add(titles[a].Groups[1].Value);
+                            lv1.SubItems.Add("http://static.sse.com.cn" + links[a].Groups[1].Value.Replace("\\",""));
+                            if (listView1.Items.Count > 2)
+                            {
+                                listView1.EnsureVisible(listView1.Items.Count - 1);  //滚动到指定位置
+                            }
+                            while (this.zanting == false)
+                            {
+                                Application.DoEvents();//如果loader是false表明正在加载,,则Application.DoEvents()意思就是处理其他消息。阻止当前的队列继续执行。
+                            }
+
+                        }
+
+
+                    }
+                
+                
+            }
+            textBox2.Text += DateTime.Now.ToString() + "抓取结束" + "\r\n";
+        }
+        #endregion
+
+        #region 深圳证券交易所
+
+        public void szse()
+        {
+            DateTime dtStart = DateTime.Parse(dateTimePicker1.Text); ;
+            DateTime dtEnd = DateTime.Parse(dateTimePicker2.Text);
+
+            string startdate = dtStart.ToString("yyyy-MM-dd");
+            string enddate = dtEnd.ToString("yyyy-MM-dd");
+
+            string[] text = textBox1.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 1; j < 50; j++)
+                {
+                    string url = "http://www.szse.cn/api/disc/announcement/annList?random=0.3188454187775951";
+                    string json = "{\"seDate\":[\""+startdate+"\",\""+enddate+"\"],\"stock\":[\""+text[i]+"\"],\"channelCode\":[\"listedNotice_disc\"],\"pageSize\":30,\"pageNum\":"+j+"}";
+                    
+                    string html = PostUrl(url,json);
+                    textBox2.Text +=DateTime.Now.ToString()+ "正在抓取........"+text[i]+"\r\n";
+                    
+                    MatchCollection dates = Regex.Matches(html, @"""publishTime"":""([\s\S]*?)""");
+                    Match name = Regex.Match(html, @"secName"":\[""([\s\S]*?)""");
+                    MatchCollection titles = Regex.Matches(html, @"""title"":""([\s\S]*?)""");
+                    MatchCollection links = Regex.Matches(html, @"""id"":""([\s\S]*?)""");
+
+                    if (dates.Count == 0)
+                        break;
+
+
+                    for (int a = 0; a < dates.Count; a++)
+                    {
+
+                        ListViewItem lv1 = listView1.Items.Add((listView1.Items.Count + 1).ToString()); //使用Listview展示数据         
+                        lv1.SubItems.Add(dates[a].Groups[1].Value);
+                        lv1.SubItems.Add(text[i]);
+                        lv1.SubItems.Add(name.Groups[1].Value);
+                        lv1.SubItems.Add(titles[a].Groups[1].Value);
+                        lv1.SubItems.Add("http://www.szse.cn/disclosure/listed/bulletinDetail/index.html?" + links[a].Groups[1].Value);
+                        if (listView1.Items.Count > 2)
+                        {
+                            listView1.EnsureVisible(listView1.Items.Count - 1);  //滚动到指定位置
+                        }
+                        while (this.zanting == false)
+                        {
+                            Application.DoEvents();//如果loader是false表明正在加载,,则Application.DoEvents()意思就是处理其他消息。阻止当前的队列继续执行。
+                        }
+
+                    }
+
+
+                }
+
+
+            }
+
+            textBox2.Text += DateTime.Now.ToString() + "抓取结束" + "\r\n";
+        }
+        #endregion
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            #region 通用验证
+
+            bool value = false;
+            string html = method.GetUrl("http://acaiji.com/success/ip.php", "utf-8");
+            string localip = method.GetIP();
+            MatchCollection ips = Regex.Matches(html, @"<td style='color:red;'>([\s\S]*?)</td>", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+            foreach (Match ip in ips)
+            {
+                if (ip.Groups[1].Value.Trim() == "12.12.12.12")
+                {
+                    value = true;
+                    break;
+                }
+
+            }
+            if (value == true)
+            {
+                if (comboBox1.Text == "上海证券交易所")
+                {
+                    Thread thread = new Thread(new ThreadStart(sse));
+                    Control.CheckForIllegalCrossThreadCalls = false;
+                    thread.Start();
+                }
+
+                else if (comboBox1.Text == "深圳证券交易所")
+                {
+                    Thread thread = new Thread(new ThreadStart(szse));
+                    Control.CheckForIllegalCrossThreadCalls = false;
+                    thread.Start();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("IP不符");
+
+            }
+            #endregion
+           
+           
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            zanting = false;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            zanting = true;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            method.DataTableToExcel(method.listViewToDataTable(this.listView1), "Sheet1", true);
         }
     }
 }
