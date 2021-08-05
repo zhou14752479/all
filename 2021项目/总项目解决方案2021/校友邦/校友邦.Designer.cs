5052bf468df9e6424ca03f0c00ed8802feeb1046
Your Name <you@example.com>
2021-08-05 15:15:56 +0800

    2021-08-05 15点15分

---
+++ 2021项目/总项目解决方案2021/校友邦/校友邦.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace 校友邦
+{
+    partial class 校友邦
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "导入账号";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.Font = new System.Drawing.Font("宋体", 11F);
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 135);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(597, 372);
+            this.listView1.TabIndex = 192;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序号";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "账号";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "密码";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "地址";
+            this.columnHeader4.Width = 200;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 31);
+            this.button2.TabIndex = 193;
+            this.button2.Text = "开始签到";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 31);
+            this.button3.TabIndex = 194;
+            this.button3.Text = "暂停/继续";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 14);
+            this.label1.TabIndex = 195;
+            this.label1.Text = "文本路径：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10F);
+            this.textBox1.Location = new System.Drawing.Point(96, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 23);
+            this.textBox1.TabIndex = 196;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "结果";
+            this.columnHeader5.Width = 200;
+            // 
+            // 校友邦
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 507);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "校友邦";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "校友邦";
+            this.Load += new System.EventHandler(this.校友邦_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
---
+++ 2021项目/总项目解决方案2021/校友邦/校友邦.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using myDLL;
+namespace 校友邦
+{
+    public partial class 校友邦 : Form
+    {
+        public 校友邦()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void 校友邦_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(openFileDialog1.FileName, method.EncodingType.GetTxtType(openFileDialog1.FileName));
+                //一次性读取完 
+                string texts = sr.ReadToEnd();
+                string[] text = texts.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    string[] value = text[i].Split(new string[] { "#" }, StringSplitOptions.None);
+                    if (value.Length > 2)
+                    {
+                        ListViewItem lv1 = listView1.Items.Add(listView1.Items.Count.ToString()); //使用Listview展示数据
+                        lv1.SubItems.Add(value[0]);
+                        lv1.SubItems.Add(value[1]);
+                        lv1.SubItems.Add(value[2]);
+                        lv1.SubItems.Add("");
+                    }
+                }
+                sr.Close();  //只关闭流
+                sr.Dispose();   //销毁流内存
+            }
+        }
+
+
+        public string login(string username,string password)
+        {
+            password = method.GetMD5(password);
+            string url = "https://xcx.xybsyw.com/login/login.action";
+            string postdata = "username="+username+"&password="+password+"&openId=ooru94lH0MDBlYKT4dUwpEkRyAWQ&deviceId=";
+            string html = method.PostUrl(url,postdata,"","utf-8", "application/x-www-form-urlencoded", "");
+           
+            string sessionId = Regex.Match(html, @"""sessionId"":""([\s\S]*?)""").Groups[1].Value;
+            if (sessionId != "")
+            {
+                return "JSESSIONID=" + sessionId + ";";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+
+        public string getplanid(string cookie)
+        {
+
+            string url = "https://xcx.xybsyw.com/student/progress/ProjectList.action";
+            string postdata = "";
+            string html = method.PostUrl(url, postdata, cookie, "utf-8", "application/x-www-form-urlencoded", "");
+            string planid = Regex.Match(html, @"""planId"":([\s\S]*?),").Groups[1].Value;
+            return planid;
+        }
+
+        public string gettraineeId(string planid,string cookie)
+        {
+
+            string url = "https://xcx.xybsyw.com/student/clock/GetPlan!getDefault.action";
+            string postdata = "planId="+planid;
+            string html = method.PostUrl(url, postdata, cookie, "utf-8", "application/x-www-form-urlencoded", "");
+            string traineeId = Regex.Match(html, @"""traineeId"":([\s\S]*?)}").Groups[1].Value;
+            return traineeId;
+        }
+
+        public string qiandao(string cookie, string address,string traineeId)
+        {
+            address = System.Web.HttpUtility.UrlEncode(address);
+            string baidumapUrl = "https://api.map.baidu.com/geocoding/v3/?address="+address+"&output=json&ak=9DemeyQjUrIX14Fz8uEwVpGyKErUP4Sb&callback=showLocation";
+            string baidumapHtml = method.GetUrl(baidumapUrl,"utf-8");
+            string lat = Regex.Match(baidumapHtml, @"lat"":([\s\S]*?)}").Groups[1].Value.Trim();
+            string lng = Regex.Match(baidumapHtml, @"lng"":([\s\S]*?),").Groups[1].Value.Trim();
+
+
+           
+            string url = "https://xcx.xybsyw.com/student/clock/Post!updateClock.action";
+            string postdata = "traineeId="+ traineeId + "&adcode=640104&lat=" + lat+"&lng="+lng+"&address="+address+"&deviceName=microsoft&punchInStatus=0&clockStatus=2";
+            string html = method.PostUrl(url, postdata, cookie, "utf-8", "application/x-www-form-urlencoded", "");
+            string msg = Regex.Match(html, @"""msg"":""([\s\S]*?)""").Groups[1].Value;
+            if (msg != "")
+            {
+                return msg;
+            }
+            else
+            {
+                return "failed";
+            }
+        }
+
+        public void run()
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                try
+                {
+                    string username = listView1.Items[i].SubItems[1].Text;
+                    string password = listView1.Items[i].SubItems[2].Text;
+                    string address = listView1.Items[i].SubItems[3].Text;
+                    string cookie = login(username,password);
+                    if (cookie == "")
+                    {
+                        listView1.Items[i].SubItems[4].Text = "登陆失败";
+                        continue;
+                    }
+                    else
+                    {
+                        string planid = getplanid(cookie);
+                        string traineeid = gettraineeId(planid,cookie);
+                      string msg=  qiandao(cookie,address, traineeid);
+                        listView1.Items[i].SubItems[4].Text = msg;
+
+                    }
+
+                    Thread.Sleep(1000);
+                    while (this.zanting == false)
+                    {
+                        Application.DoEvents();//如果loader是false表明正在加载,,则Application.DoEvents()意思就是处理其他消息。阻止当前的队列继续执行。
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        Thread thread;
+        bool zanting = true;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (thread == null || !thread.IsAlive)
+            {
+                thread = new Thread(run);
+                thread.Start();
+                Control.CheckForIllegalCrossThreadCalls = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (zanting == false)
+            {
+
+                zanting = true;
+            }
+            else
+            {
+                zanting = false;
+            }
+        }
+    }
+}
