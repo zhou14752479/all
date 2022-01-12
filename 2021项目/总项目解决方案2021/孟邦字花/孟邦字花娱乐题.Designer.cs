f9c3b8b10fc8a00130d7e28c82b8e29ccd8e2e1c
Your Name <you@example.com>
2022-01-12 21:03:10 +0800

    1--12

---
+++ 2021项目/总项目解决方案2021/孟邦字花/孟邦字花娱乐题.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace 孟邦字花
+{
+    partial class 孟邦字花娱乐题
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1163, 49);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 49);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1163, 636);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.linkLabel1.Font = new System.Drawing.Font("宋体", 10F);
+            this.linkLabel1.Location = new System.Drawing.Point(1128, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(35, 14);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "关闭";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 11F);
+            this.button1.Location = new System.Drawing.Point(566, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "刷新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 孟邦字花娱乐题
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 685);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "孟邦字花娱乐题";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "孟邦字花娱乐题";
+            this.Load += new System.EventHandler(this.孟邦字花娱乐题_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 2021项目/总项目解决方案2021/孟邦字花/孟邦字花娱乐题.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 孟邦字花
+{
+    public partial class 孟邦字花娱乐题 : Form
+    {
+        public 孟邦字花娱乐题()
+        {
+            InitializeComponent();
+        }
+
+        private void 孟邦字花娱乐题_Load(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+            SetFeatures(11000);
+           
+        }
+
+
+        #region 获取txt编码
+        //调用：EncodingType.GetTxtType(textBox1.Text)
+        public class EncodingType
+        {
+            /// <summary> 
+            /// 给定文件的路径，读取文件的二进制数据，判断文件的编码类型 
+            /// </summary> 
+            /// <param name=“FILE_NAME“>文件路径</param> 
+            /// <returns>文件的编码类型</returns> 
+            public static System.Text.Encoding GetTxtType(string FILE_NAME)
+            {
+                FileStream fs = new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read);
+                Encoding r = GetType(fs);
+                fs.Close();
+                return r;
+            }
+
+            /// <summary> 
+            /// 通过给定的文件流，判断文件的编码类型 
+            /// </summary> 
+            /// <param name=“fs“>文件流</param> 
+            /// <returns>文件的编码类型</returns> 
+            public static System.Text.Encoding GetType(FileStream fs)
+            {
+                byte[] Unicode = new byte[] { 0xFF, 0xFE, 0x41 };
+                byte[] UnicodeBIG = new byte[] { 0xFE, 0xFF, 0x00 };
+                byte[] UTF8 = new byte[] { 0xEF, 0xBB, 0xBF }; //带BOM 
+                Encoding reVal = Encoding.Default;
+
+                BinaryReader r = new BinaryReader(fs, System.Text.Encoding.Default);
+                int i;
+                int.TryParse(fs.Length.ToString(), out i);
+                byte[] ss = r.ReadBytes(i);
+                if (IsUTF8Bytes(ss) || (ss[0] == 0xEF && ss[1] == 0xBB && ss[2] == 0xBF))
+                {
+                    reVal = Encoding.UTF8;
+                }
+                else if (ss[0] == 0xFE && ss[1] == 0xFF && ss[2] == 0x00)
+                {
+                    reVal = Encoding.BigEndianUnicode;
+                }
+                else if (ss[0] == 0xFF && ss[1] == 0xFE && ss[2] == 0x41)
+                {
+                    reVal = Encoding.Unicode;
+                }
+                r.Close();
+                return reVal;
+
+            }
+
+            /// <summary> 
+            /// 判断是否是不带 BOM 的 UTF8 格式 
+            /// </summary> 
+            /// <param name=“data“></param> 
+            /// <returns></returns> 
+            private static bool IsUTF8Bytes(byte[] data)
+            {
+                int charByteCounter = 1; //计算当前正分析的字符应还有的字节数 
+                byte curByte; //当前分析的字节. 
+                for (int i = 0; i < data.Length; i++)
+                {
+                    curByte = data[i];
+                    if (charByteCounter == 1)
+                    {
+                        if (curByte >= 0x80)
+                        {
+                            //判断当前 
+                            while (((curByte <<= 1) & 0x80) != 0)
+                            {
+                                charByteCounter++;
+                            }
+                            //标记位首位若为非0 则至少以2个1开始 如:110XXXXX...........1111110X 
+                            if (charByteCounter == 1 || charByteCounter > 6)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //若是UTF-8 此时第一位必须为1 
+                        if ((curByte & 0xC0) != 0x80)
+                        {
+                            return false;
+                        }
+                        charByteCounter--;
+                    }
+                }
+                if (charByteCounter > 1)
+                {
+                    throw new Exception("非预期的byte格式");
+                }
+                return true;
+            }
+        }
+
+        #endregion
+
+        #region 修改注册表信息使WebBrowser使用指定版本IE内核 传入11000是IE11
+        public static void SetFeatures(UInt32 ieMode)
+        {
+            //传入11000是IE11, 9000是IE9, 只不过当试着传入6000时, 理应是IE6, 可实际却是Edge, 这时进一步测试, 当传入除IE现有版本以外的一些数值时WebBrowser都使用Edge内核
+            if (LicenseManager.UsageMode != LicenseUsageMode.Runtime)
+            {
+                throw new ApplicationException();
+            }
+            //获取程序及名称
+            string appName = System.IO.Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            string featureControlRegKey = "HKEY_CURRENT_USER\\Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\";
+            //设置浏览器对应用程序(appName)以什么模式(ieMode)运行
+            Registry.SetValue(featureControlRegKey + "FEATURE_BROWSER_EMULATION", appName, ieMode, RegistryValueKind.DWord);
+            //不晓得设置有什么用
+            Registry.SetValue(featureControlRegKey + "FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", appName, 1, RegistryValueKind.DWord);
+        }
+        #endregion
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("确定要关闭吗？", "关闭", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (dr == DialogResult.OK)
+            {
+                // Environment.Exit(0);
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+            else
+            {
+             
+            }
+        }
+
+        public string getshici()
+        {
+            try
+            {
+                Random rd = new Random(Guid.NewGuid().GetHashCode()); //生成不重复的随机数，默认的话根据时间戳如果太快会相同
+
+                StreamReader sr = new StreamReader(Application.StartupPath + @"\data\shici.txt", EncodingType.GetTxtType(Application.StartupPath + @"\data\shici.txt"));
+                //一次性读取完 
+                string html = sr.ReadToEnd();
+                string[] text = html.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                int suiji = rd.Next(1, text.Length - 2);
+                sr.Close();
+                return text[suiji];
+            }
+            catch (Exception ex)
+            {
+
+               return "明日登峰须造极，渺观宇宙我心宽。";
+            }
+           
+        }
+        public string getchengyu()
+        {
+            try
+            {
+                Random rd = new Random(Guid.NewGuid().GetHashCode()); //生成不重复的随机数，默认的话根据时间戳如果太快会相同
+
+                StreamReader sr = new StreamReader(Application.StartupPath + @"\data\chengyu.txt", EncodingType.GetTxtType(Application.StartupPath + @"\data\chengyu.txt"));
+                //一次性读取完 
+                string html = sr.ReadToEnd();
+                string[] text = html.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                int suiji = rd.Next(1, text.Length - 2);
+                sr.Close();
+                return text[suiji];
+            }
+            catch (Exception ex)
+            {
+
+                return "一往无前";
+            }
+
+        }
+        public void run()
+        {
+
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 7; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+
+                    }
+                }
+
+                StreamReader sr = new StreamReader(Application.StartupPath + @"\index\index.html", EncodingType.GetTxtType(Application.StartupPath + @"\index\index.html"));
+                //一次性读取完 
+                string body = sr.ReadToEnd();
+
+                sr.Close();  //只关闭流
+                sr.Dispose();   //销毁流内存
+                System.IO.File.WriteAllText(Application.StartupPath + @"\index\index.html", body, Encoding.UTF8);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            MessageBox.Show(getshici());
+            webBrowser1.Refresh();
+        }
+    }
+}
