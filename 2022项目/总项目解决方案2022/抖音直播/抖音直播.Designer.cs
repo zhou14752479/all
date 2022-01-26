fdc3940eb79366f8bc530f38687edabd74de55b6
Your Name <you@example.com>
2022-01-26 20:55:30 +0800

    1--26

---
+++ 2022项目/总项目解决方案2022/抖音直播/抖音直播.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace 抖音直播
+{
+    partial class 抖音直播
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time_label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name_txt = new System.Windows.Forms.TextBox();
+            this.shi_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.time_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.time_label.Location = new System.Drawing.Point(262, 724);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(214, 27);
+            this.time_label.TabIndex = 0;
+            this.time_label.Text = "2022-01-26 20:18:30";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(812, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "欢迎来到思忆抖音直播间，点击关注，弹幕发送姓名，系统自动通过大数据进行姓名作诗！";
+            // 
+            // name_txt
+            // 
+            this.name_txt.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Bold);
+            this.name_txt.Location = new System.Drawing.Point(277, 101);
+            this.name_txt.Name = "name_txt";
+            this.name_txt.Size = new System.Drawing.Size(219, 38);
+            this.name_txt.TabIndex = 3;
+            this.name_txt.Text = "周凯";
+            this.name_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // shi_txt
+            // 
+            this.shi_txt.Font = new System.Drawing.Font("微软雅黑 Light", 50F, System.Drawing.FontStyle.Italic);
+            this.shi_txt.Location = new System.Drawing.Point(32, 164);
+            this.shi_txt.Multiline = true;
+            this.shi_txt.Name = "shi_txt";
+            this.shi_txt.Size = new System.Drawing.Size(768, 403);
+            this.shi_txt.TabIndex = 4;
+            this.shi_txt.Text = "周公不为公 ，\r\n凯入系名王 。\r\n周公不为公 ，\r\n凯入系名王 。";
+            this.shi_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 20F);
+            this.label3.Location = new System.Drawing.Point(178, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "姓名：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 20F);
+            this.label4.Location = new System.Drawing.Point(512, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 27);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "作诗：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(812, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "欢迎来到思忆抖音直播间，点击关注，弹幕发送姓名，系统自动通过大数据进行姓名作诗！";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(611, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 抖音直播
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(842, 760);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.shi_txt);
+            this.Controls.Add(this.name_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.time_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "抖音直播";
+            this.Text = "抖音直播";
+            this.Load += new System.EventHandler(this.抖音直播_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.抖音直播_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.抖音直播_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time_label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name_txt;
+        private System.Windows.Forms.TextBox shi_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 2022项目/总项目解决方案2022/抖音直播/抖音直播.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 抖音直播
+{
+    public partial class 抖音直播 : Form
+    {
+        public 抖音直播()
+        {
+            InitializeComponent();
+        }
+
+        #region POST默认请求
+        /// <summary>
+        /// POST请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送的数据包</param>
+        /// <param name="COOKIE">cookie</param>
+        /// <param name="charset">编码格式</param>
+        /// <returns></returns>
+        public static string PostUrlDefault(string url, string postData, string COOKIE)
+        {
+            try
+            {
+
+                string charset = "utf-8";
+                string html = "";
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; //获取不到加上这一条
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "Post";
+                request.Proxy = null;//防止代理抓包
+                //添加头部
+                //WebHeaderCollection headers = request.Headers;
+                //headers.Add("sec-fetch-mode:navigate");
+                //headers.Add("sec-fetch-site:same-origin");
+                //headers.Add("sec-fetch-user:?1");
+                //headers.Add("upgrade-insecure-requests: 1");
+                //添加头部
+                request.ContentType = "application/x-www-form-urlencoded";
+                // request.Accept = "application/json, text/javascript, */*; q=0.01"; //返回中文问号参考
+                //request.ContentType = "application/json";
+                request.ContentLength = Encoding.UTF8.GetBytes(postData).Length;
+                // request.ContentLength = postData.Length;
+                request.Headers.Add("Accept-Encoding", "gzip");
+                request.AllowAutoRedirect = false;
+                request.KeepAlive = true;
+
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
+                request.Headers.Add("Cookie", COOKIE);
+
+                request.Referer = url;
+                StreamWriter sw = new StreamWriter(request.GetRequestStream());
+                sw.Write(postData);
+                sw.Flush();
+
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;  //获取反馈
+                response.GetResponseHeader("Set-Cookie");
+
+                if (response.Headers["Content-Encoding"] == "gzip")
+                {
+
+                    GZipStream gzip = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);//解压缩
+                    StreamReader reader = new StreamReader(gzip, Encoding.GetEncoding(charset));
+                    html = reader.ReadToEnd();
+                    reader.Close();
+                }
+                else
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(charset)); //reader.ReadToEnd() 表示取得网页的源码流 需要引用 using  IO
+                    html = reader.ReadToEnd();
+                    reader.Close();
+                }
+
+
+                response.Close();
+                return html;
+            }
+            catch (WebException ex)
+            {
+
+                return ex.ToString();
+            }
+
+
+        }
+
+        #endregion
+
+        private Point mPoint = new Point();
+        private void 抖音直播_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            System.Timers.Timer t = new System.Timers.Timer(1000);//实例化Timer类，设置间隔时间为10000毫秒；
+            t.Elapsed += new System.Timers.ElapsedEventHandler(settime);//到达时间的时候执行事件；
+            t.AutoReset = true;//设置是执行一次（false）还是一直执行(true)；
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+        }
+        public void settime(object source, System.Timers.ElapsedEventArgs e)
+        {
+
+            time_label.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+          
+        }
+
+        
+        public void zuoshi()
+        {
+            try
+            {
+                string url = "http://jiqie.zhenbi.com/5/re34.php";
+                string name= System.Web.HttpUtility.UrlEncode(name_txt.Text);
+                string postdata= "id="+name+"&zhenbi=20191123&id1=5&id2=1";
+                string html = PostUrlDefault(url,postdata,"");
+                string shi = Regex.Match(html, @"<span class=""bga1""><a>([\s\S]*?)</a>").Groups[1].Value.Replace("<br/>", "\r\n");
+                // string[] text = shi.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                shi_txt.Text = shi;
+            }
+            catch (Exception ex)
+            {
+
+                ;
+            }
+
+        }
+
+
+
+        private void 抖音直播_MouseDown(object sender, MouseEventArgs e)
+        {
+            mPoint.X = e.X;
+            mPoint.Y = e.Y;
+        }
+
+        private void 抖音直播_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point myPosittion = MousePosition;
+                myPosittion.Offset(-mPoint.X, -mPoint.Y);
+                Location = myPosittion;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zuoshi();
+        }
+    }
+}
